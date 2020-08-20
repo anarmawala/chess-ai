e2de8e4c73ee6959feda78a088971ffa03d01833
Arshad Narmawala <arshadn00@gmail.com>
2020-08-20 15:15:49 -0500

    ✨ feat/Added move log

--- src/chess/change.rs
+++ src/chess/change.rs
@@ -2,11 +2,11 @@ use super::piece::Piece;
 
 #[derive(Copy, Clone)]
 pub struct Change {
-	old_row: usize,
-	old_col: usize,
+    old_row: usize,
+    old_col: usize,
 
-	new_row: usize,
-	new_col: usize,
+    new_row: usize,
+    new_col: usize,
 
-	piece: Piece,
+    piece: Piece,
 }
--- src/chess/piece.rs
+++ src/chess/piece.rs
@@ -5,57 +5,69 @@ use super::pieces::Pieces::{BISHOP, EMPTY, HORSE, KING, PAWN, QUEEN, ROOK};
 
 #[derive(Copy, Clone)]
 pub struct Piece {
-	// * Piece's type and color
-	color: Colors,
-	title: Pieces,
+    // * Piece's type and color
+    color: Colors,
+    title: Pieces,
 
-	// * Piece game info
-	moved: bool,
-	value: i32,
+    // * Piece game info
+    moved: bool,
+    value: i32,
 }
 
 impl Piece {
-	pub fn new(color: Colors, title: Pieces) -> Self {
-		// * Value of the piece
-		let _value = match title {
-			PAWN => 1,
-			QUEEN => 9,
-			ROOK => 5,
-			HORSE => 3,
-			BISHOP => 3,
-			EMPTY | KING => 0,
-		};
+    pub fn new(color: Colors, title: Pieces) -> Self {
+        // * Value of the piece
+        let _value = match title {
+            PAWN => 1,
+            QUEEN => 9,
+            ROOK => 5,
+            HORSE => 3,
+            BISHOP => 3,
+            EMPTY | KING => 0,
+        };
 
-		// * Multiplier of the value to allow fro easy summation scoring
-		let _multiplier = match color {
-			WHITE => 1,
-			BLACK => -1,
-			NONE => 0,
-		};
+        // * Multiplier of the value to allow fro easy summation scoring
+        let _multiplier = match color {
+            WHITE => 1,
+            BLACK => -1,
+            NONE => 0,
+        };
 
-		// * Return the created Piece
-		Piece {
-			moved: false,
-			color,
-			title,
+        // * Return the created Piece
+        Piece {
+            moved: false,
+            color,
+            title,
 
-			value: _value * _multiplier,
-		}
-	}
+            value: _value * _multiplier,
+        }
+    }
 
-	pub fn color(&self) -> &Colors {
-		&self.color
-	}
+    pub fn color(&self) -> &Colors {
+        &self.color
+    }
 
-	pub fn title(&self) -> &Pieces {
-		&self.title
-	}
+    pub fn title(&self) -> &Pieces {
+        &self.title
+    }
 
-	pub fn moved(&self) -> bool {
-		self.moved
-	}
+    pub fn moved(&self) -> bool {
+        self.moved
+    }
 
-	pub fn value(&self) -> i32 {
-		self.value
-	}
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+
+    pub fn get_moves(&self, row: usize, col: usize) {
+        match self.title {
+            PAWN => {}
+            KING => {}
+            QUEEN => {}
+            HORSE => {}
+            ROOK => {}
+            BISHOP => {}
+            EMPTY => panic!("No piece to be moved"),
+        }
+    }
 }
