96492b1417e8da56d6804ee4e96fd3f6c5bf69c0
Arshad Narmawala <arshadn00@gmail.com>
2020-08-20 14:50:16 -0500

    🎨 chore/Format Files

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,20 @@ mod chess;
 use chess::Game;
 
 fn main() {
-    let game: Game = Game::new(true, 5);
-    let board = game.board();
+	let game: Game = Game::new(true, 5);
+	let board = game.board();
 
-    for _row in (0..8).rev() {
-        for _column in 0..8 {
-            let _piece = board[_row * 8 + _column];
+	for _row in (0..8).rev() {
+		for _column in 0..8 {
+			let _piece = board[_row * 8 + _column];
 
-            // print!("{:^5?} ", piece.row());
-            // print!("{:^5?} ", piece.col());
-            // print!("{:^5?} ", piece.title());
-            // print!("{:^5?} ", piece.value());
-            // print!("{:^5?} ", piece.color());
-            // print!("{:^5?} ", piece.moved());
-        }
-        println!("\n");
-    }
+			// print!("{:^5?} ", piece.row());
+			// print!("{:^5?} ", piece.col());
+			// print!("{:^5?} ", piece.title());
+			// print!("{:^5?} ", piece.value());
+			// print!("{:^5?} ", piece.color());
+			// print!("{:^5?} ", piece.moved());
+		}
+		println!("\n");
+	}
 }
