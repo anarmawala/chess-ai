ac42f878aedca0f93b4771ef11b7ed3deb85e7d4
Arshad Narmawala <arshadn00@gmail.com>
2020-08-20 14:50:50 -0500

    🔥 chore/Replace move number with vec

--- src/chess/mod.rs
+++ src/chess/mod.rs
@@ -9,64 +9,64 @@ pub use pieces::Pieces;
 pub use pieces::Pieces::{BISHOP, EMPTY, HORSE, KING, PAWN, QUEEN, ROOK};
 
 pub struct Game {
-    // * Chess Board
-    board: Vec<Piece>,
+	// * Chess Board
+	board: Vec<Piece>,
 
-    // * Player preferences
-    player_white: bool,
-    difficulty: u8,
+	// * Log of moves
+	moves: Vec<()>,
 
-    // * Game state variables
-    num_move: usize,
+	// * Player preferences
+	player_white: bool,
+	difficulty: u8,
 }
 
 impl Game {
-    // * Initialize the game board
-    pub fn new(play_first: bool, difficulty: u8) -> Self {
-        let mut game_board = Vec::new();
-        for _row in 1..=8 {
-            match _row {
-                2 | 7 => {
-                    // ? Decide the color base on row number
-                    let _color = if _row <= 2 { WHITE } else { BLACK };
+	// * Initialize the game board
+	pub fn new(play_first: bool, difficulty: u8) -> Self {
+		let mut game_board = Vec::new();
+		for _row in 1..=8 {
+			match _row {
+				2 | 7 => {
+					// ? Decide the color base on row number
+					let _color = if _row <= 2 { WHITE } else { BLACK };
 
-                    // * Add 8 pawns
-                    for _i in 1..=8 {
-                        game_board.push(Piece::new(_color, PAWN, _row, _i))
-                    }
-                }
-                1 | 8 => {
-                    // ? Decide the color base on row number
-                    let _color = if _row <= 1 { WHITE } else { BLACK };
+					// * Add 8 pawns
+					for _i in 1..=8 {
+						game_board.push(Piece::new(_color, PAWN, _row, _i))
+					}
+				}
+				1 | 8 => {
+					// ? Decide the color base on row number
+					let _color = if _row <= 1 { WHITE } else { BLACK };
 
-                    game_board.push(Piece::new(_color, ROOK, _row, 1));
-                    game_board.push(Piece::new(_color, HORSE, _row, 2));
-                    game_board.push(Piece::new(_color, BISHOP, _row, 3));
-                    game_board.push(Piece::new(_color, QUEEN, _row, 4));
-                    game_board.push(Piece::new(_color, KING, _row, 5));
-                    game_board.push(Piece::new(_color, BISHOP, _row, 6));
-                    game_board.push(Piece::new(_color, HORSE, _row, 7));
-                    game_board.push(Piece::new(_color, ROOK, _row, 8));
-                }
-                3 | 4 | 5 | 6 => {
-                    // * Add 8 empty blocks
-                    for _i in 1..=8 {
-                        game_board.push(Piece::new(NONE, EMPTY, _row, _i))
-                    }
-                }
-                _ => panic!("Error, Chess only has eight rows"),
-            }
-        }
+					game_board.push(Piece::new(_color, ROOK, _row, 1));
+					game_board.push(Piece::new(_color, HORSE, _row, 2));
+					game_board.push(Piece::new(_color, BISHOP, _row, 3));
+					game_board.push(Piece::new(_color, QUEEN, _row, 4));
+					game_board.push(Piece::new(_color, KING, _row, 5));
+					game_board.push(Piece::new(_color, BISHOP, _row, 6));
+					game_board.push(Piece::new(_color, HORSE, _row, 7));
+					game_board.push(Piece::new(_color, ROOK, _row, 8));
+				}
+				3 | 4 | 5 | 6 => {
+					// * Add 8 empty blocks
+					for _i in 1..=8 {
+						game_board.push(Piece::new(NONE, EMPTY, _row, _i))
+					}
+				}
+				_ => panic!("Error, Chess only has eight rows"),
+			}
+		}
 
-        Game {
-            player_white: play_first,
-            difficulty,
-            num_move: 0,
-            board: game_board,
-        }
-    }
+		Game {
+			player_white: play_first,
+			difficulty,
+			moves: Vec::new(),
+			board: game_board,
+		}
+	}
 
-    pub fn board(&self) -> &Vec<Piece> {
-        &self.board
-    }
+	pub fn board(&self) -> &Vec<Piece> {
+		&self.board
+	}
 }
